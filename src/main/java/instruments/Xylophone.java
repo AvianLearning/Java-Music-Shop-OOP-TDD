c8d5c02e8c41985b88030d2d1e637cc132e3e81d
AvianLearning <iaradcliffe@hotmail.com>
2020-11-28 20:43:41 +0000

    Added Xylophone class and tests for properties and play method

--- src/main/java/instruments/Xylophone.java
+++ src/main/java/instruments/Xylophone.java
@@ -1,4 +1,18 @@
 package instruments;
 
-public class Xylophone {
+public class Xylophone extends Instrument {
+    private int numberOfBars;
+
+    public Xylophone(String type, String colour, String material, String make, String model, int numberOfBars) {
+        super(type, colour, material, make, model);
+        this.numberOfBars = numberOfBars;
+    }
+
+    public int getNumberOfBars() {
+        return numberOfBars;
+    }
+
+    public String play() {
+        return "Plinkety plinkety plonk plonk";
+    }
 }
--- src/test/java/XylophoneTest.java
+++ src/test/java/XylophoneTest.java
@@ -1,2 +1,26 @@
+import instruments.Xylophone;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class XylophoneTest {
+
+    Xylophone xylophone;
+
+    @Before
+    public void setUp() {
+        xylophone = new Xylophone("percussion", "Brown", "African Padauk wood", "WHD", "Orchestral", 37);
+    }
+
+    @Test
+    public void canGetNumberOfBars() {
+        assertEquals(37, xylophone.getNumberOfBars());
+    }
+
+    @Test
+    public void canHearXylophonePlay() {
+        assertEquals("Plinkety plinkety plonk plonk", xylophone.play());
+    }
 }
