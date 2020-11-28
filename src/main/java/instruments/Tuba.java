07817e328f05c826aace4763a66530bea9503ff4
AvianLearning <iaradcliffe@hotmail.com>
2020-11-28 20:36:05 +0000

    Created tuba class and tested can get properties and play method

--- src/main/java/instruments/Tuba.java
+++ src/main/java/instruments/Tuba.java
@@ -1,4 +1,18 @@
 package instruments;
 
-public class Tuba {
+public class Tuba extends Instrument {
+    private int numberOfValves;
+
+    public Tuba(String type, String colour, String material, String make, String model, int numberOfValves) {
+        super(type, colour, material, make, model);
+        this.numberOfValves = numberOfValves;
+    }
+
+    public int getNumberOfValves() {
+        return numberOfValves;
+    }
+
+    public String play() {
+        return "Pum pum pum pumpedy rum pum pum";
+    }
 }
--- src/test/java/TubaTest.java
+++ src/test/java/TubaTest.java
@@ -1,2 +1,26 @@
+import instruments.Tuba;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class TubaTest {
+
+    Tuba tuba;
+
+    @Before
+    public void setUp() {
+        tuba = new Tuba("brass", "Golden", "brass", "Yamaha", "YBB- 641", 4);
+    }
+
+    @Test
+    public void canGetNumberOfValves() {
+        assertEquals(4, tuba.getNumberOfValves());
+    }
+
+    @Test
+    public void canHearTubaPlay() {
+        assertEquals("Pum pum pum pumpedy rum pum pum", tuba.play());
+    }
 }
